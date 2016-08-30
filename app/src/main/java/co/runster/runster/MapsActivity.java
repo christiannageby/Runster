ac38a214c4b81c78a4a4290e4dc2fddcd2e3a3cd
christiannageby <christian.nageby@gmail.com>
2016-08-30 18:39:33 +0200

    Configurera Kartan, ADB-Debugging & förberett för firebase

--- app/src/main/java/co/runster/runster/MapsActivity.java
+++ app/src/main/java/co/runster/runster/MapsActivity.java
@@ -2,6 +2,7 @@ package co.runster.runster;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +23,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -43,4 +45,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+
 }
