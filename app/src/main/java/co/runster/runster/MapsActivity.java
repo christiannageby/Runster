2249d353c624d3c9612f1e5f2477182f84583003
unefined <christian.angeby@gmail.com>
2016-09-01 13:46:59 +0200

    Fixat rörelse i meter

--- app/src/main/java/co/runster/runster/MapsActivity.java
+++ app/src/main/java/co/runster/runster/MapsActivity.java
@@ -20,13 +20,43 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationChangeListener {
 
     private GoogleMap map;
+
+    public LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    public Criteria criteria = new Criteria();
+    public String provider = locationManager.getBestProvider(criteria, true);
+
+    public Location lastLocation = locationManager.getLastKnownLocation(provider);
+
+    public LatLng lastPos = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+    public float distance = 0;
+
+    //få avståndet mellan 2 punkter i meter
+    public float getDistance(LatLng my_latlong, LatLng frnd_latlong) {
+        Location l1 = new Location("One");
+        l1.setLatitude(my_latlong.latitude);
+        l1.setLongitude(my_latlong.longitude);
+
+        Location l2 = new Location("Two");
+        l2.setLatitude(frnd_latlong.latitude);
+        l2.setLongitude(frnd_latlong.longitude);
+
+        return l1.distanceTo(l2);
+    }
 
     @Override
     //vad som händer när appen startas
     protected void onCreate(Bundle savedInstanceState) {
+        //kolla gps-postionen och gps-status
+        //region CHECK_GPS_STATUS
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        //endregion
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
@@ -60,18 +90,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //vad som händer med kartan
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
-        //region CHECK_GPS_STATUS
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        //endregion
-
-        Location lastLocation = locationManager.getLastKnownLocation(provider);
         map.addMarker(new MarkerOptions().position(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())));
     }
 
+    @Override
+    public void onMyLocationChange(Location location) {
+        distance++;
+        lastPos = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+    }
 }
