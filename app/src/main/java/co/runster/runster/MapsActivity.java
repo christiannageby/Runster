d062f4b285a3ddc977639f1537c24a0c55630677
unefined <christian.angeby@gmail.com>
2016-09-07 09:52:07 +0200

    Visa nuvarande porition på karta, samt vad som händer när man trycker på menuknappen

--- app/src/main/java/co/runster/runster/MapsActivity.java
+++ app/src/main/java/co/runster/runster/MapsActivity.java
@@ -10,12 +10,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -23,10 +20,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationSource.OnLocationChangedListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap map;
-    public Marker myMkr;
+    public Marker YouPos;
 
     @Override
     //vad som händer när appen startas
@@ -49,26 +46,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         //endregion
-
-        //region MENU_BUTTON_CLICK
-        //definera en Floation button till menyknappen
-
-        final FloatingActionButton menu = (FloatingActionButton) findViewById(R.id.menuBtn);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //vad som ska hända när man trycker på kanppen
-                moveMkr(new LatLng(1, 1));
-            }
-        });
-        //endregion
-
     }
 
     //denna funktion flyttar användarens positionsindikator till en  ny postion
     public void moveMkr(LatLng newLocation){
-        myMkr.setPosition(newLocation);
+        YouPos.setPosition(newLocation);
         map.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
+    }
+
+    public void menuClick(View view){
+        moveMkr(new LatLng(1,2));
     }
 
     @Override
@@ -88,20 +75,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         Location lastLocation = locationManager.getLastKnownLocation(provider);
 
-        myMkr = map.addMarker(new MarkerOptions().position(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))
-                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker_i)));
 
-        //flyttar kameran till användarens position
-        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())));
+        //initiera spelarens
+        YouPos = map.addMarker(new MarkerOptions().position(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))
+               .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker_i)));
+
 
         //sätter zoom nivån till 17.0
         map.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+
+
     }
 
     //vad som händer när användaren förflyttar sig
-
-    @Override
-    public void onLocationChanged(Location location) {
-        moveMkr(new LatLng(location.getLatitude(), location.getLongitude()));
-    }
 }
