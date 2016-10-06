10d3690a1388cd52e6c8a3276b7923c3f2aaa797
unefined <christian.angeby@gmail.com>
2016-10-06 15:54:36 +0200

    Uppladdning av kod som fans på datorn

--- app/src/main/java/co/runster/runster/MapsActivity.java
+++ app/src/main/java/co/runster/runster/MapsActivity.java
@@ -1,16 +1,25 @@
 package co.runster.runster;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
@@ -21,22 +30,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationSource.OnLocationChangedListener{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
     private GoogleMap map;
     public Marker YouPos;
 
     //Initiera en ny PositionManager som används för att komma åt enhetens platsinfo
-    public LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    public LocationManager locationManager;
     //Initiera en kriterie variabel
-    public Criteria criteria = new Criteria();
+    public Criteria criteria;
 
     //få den bästa gps leverantören
-    public String provider = locationManager.getBestProvider(criteria, true);
+    public String provider;
     //få den senaste positionen uppdaterat var 5e sekund
-    public Location lastLocation = locationManager.getLastKnownLocation(provider);
-
-
+    public Location lastLocation;
 
     @Override
     //vad som händer när appen startas
@@ -51,6 +58,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        lastLocation = locationManager.getLastKnownLocation(provider);
+
         //region MAP_SEGMENT_SETUP
 
         //Välj map-segmentet och använd det till kartan
@@ -60,6 +72,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //endregion
     }
+
 
     //denna funktion flyttar användarens positionsindikator till en  ny postion
     public void moveMkr(LatLng newLocation){
@@ -80,18 +93,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //initiera spelarens positionsmarkör
         YouPos = map.addMarker(new MarkerOptions().position(
-                //Nuvarande position(När appen startas)
-                new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())
-        ).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker_i)));
+        new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker_i)));
 
         //sätter zoom nivån till 17.0
-        map.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+        map.animateCamera(CameraUpdateFactory.zoomTo(27.0f));
+
+        //flytta kameran & markern till nuvarnade kordinater
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())));
     }
 
-    //vad som händer när användaren förflyttar sig
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(getApplicationContext(), "The application was closed because the gps signal is unavailable", Toast.LENGTH_LONG)
+                .show();
+        System.exit(1);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
     @Override
     public void onLocationChanged(Location location) {
-
+        Toast.makeText(getApplicationContext(),
+                "Latitude: " + location.getLatitude() + " Longitude: " + location.getLongitude(), Toast.LENGTH_LONG).show();
     }
-
 }
